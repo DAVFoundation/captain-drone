f8effe28271526bd2f8a4d3ceae0fcca93a42e45
srfrnk <srfrnk@gmail.com>
2020-01-19 15:07:23 +0200

    When drone accepts a bid, start moving it's position toward the charger. Update its position on the map. Only when it gets to charger transition to next phase.

--- web-ui/src/app/drone/drone.component.ts
+++ web-ui/src/app/drone/drone.component.ts
@@ -11,20 +11,29 @@ export class DroneComponent implements OnInit {
   address: string;
   lat: number;
   lon: number;
-  token: string;
+  token: string = null;
   bids: BidData[] = [];
   selectedBidId: BidData = null;
-  status: string;
+  status: string = null;
   accepting = false;
   charger = null;
+  drone = {
+    lat: 32.0494226,
+    lon: 34.7636385
+  };
+  droneSpeed = {
+    lat: 0,
+    lon: 0,
+  };
   balance: string;
   logs: string[];
+  arrivedAtCharger = false;
 
   constructor(private server: ServerService) {
     this.address = '0x96De2B9394bA1894A3a717a75536E9e2d0d1Ec22';
-    this.lat = 32.0494226;
-    this.lon = 34.7636385;
     this.logs = [];
+    this.lat = this.drone.lat;
+    this.lon = this.drone.lon;
   }
 
   async ngOnInit() {
@@ -36,6 +45,12 @@ export class DroneComponent implements OnInit {
         this.balance = res.balance;
         if (this.status === 'Moving') {
           this.accepting = false;
+          if (Math.abs(this.drone.lat - this.charger.lat) < 0.0001 && Math.abs(this.drone.lon - this.charger.long) < 0.0001) {
+            this.arrivedAtCharger = true;
+          } else {
+            this.drone.lat += this.droneSpeed.lat;
+            this.drone.lon += this.droneSpeed.lon;
+          }
         }
         if (this.status === 'Waiting' && !this.accepting) {
           this.bids = (await this.server.getBids(this.token).toPromise());
@@ -56,11 +71,18 @@ export class DroneComponent implements OnInit {
       lat: this.lat.toString(),
       lon: this.lon.toString(),
     }).toPromise();
+    this.drone = {
+      lat: this.lat,
+      lon: this.lon
+    };
   }
 
   async accept(bidDataId: string) {
     const bid = this.bids.find((b) => b.id === bidDataId);
     this.charger = bid.entranceLocation;
+    this.droneSpeed.lat = (this.charger.lat - this.drone.lat) / 60.0;
+    this.droneSpeed.lon = (this.charger.long - this.drone.lon) / 60.0;
+
     this.accepting = true;
     await this.server.accept(this.token, bidDataId).toPromise();
   }
@@ -74,6 +96,7 @@ export class DroneComponent implements OnInit {
     this.token = null;
     this.status = '';
     this.charger = null;
+    this.arrivedAtCharger = false;
   }
 
   trackByLatLon(i: number, bidData: BidData) {
